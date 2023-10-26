cbf73137fd69a061a8f7bd08e397ec726f211d14
ArianaRusso <1214Unicamp!>
2023-10-26 15:15:53 -0300

    adicionando alterações em 'Account'

--- src/main/java/br/com/arianarusso/entities/Account.java
+++ src/main/java/br/com/arianarusso/entities/Account.java
@@ -2,6 +2,7 @@ package br.com.arianarusso.entities;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Random;
 import java.util.UUID;
 
 public class Account {
@@ -11,31 +12,40 @@ public class Account {
     private UUID idCustomer;
     private BigDecimal balance;
 
-    public Account(UUID id, String number, UUID idCustomer) {
+    public Account(UUID id, UUID idCustomer) {
         this.id = id;
-        this.number = number;
+        this.number = generateRandomAccountNumber();
         this.idCustomer = idCustomer;
         this.balance = new BigDecimal(BigInteger.ZERO);
+    }
+
+    private String generateRandomAccountNumber() {
+        Random random = new Random();
+        int randomNumber = 100_000 + random.nextInt(900_000);
+        return String.valueOf(randomNumber);
     }
 
     public String getNumber() {
         return number;
     }
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
     public UUID getIdCustomer() {
         return idCustomer;
-    }
-
-    public void setIdCustomer(UUID idCustomer) {
-        this.idCustomer = idCustomer;
     }
 
     public BigDecimal getBalance() {
         return balance;
     }
 
+    private void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public void addFunds(BigDecimal funds){
+        setBalance(this.balance.add(funds));
+    }
+
+    public void subtractFunds(BigDecimal funds){
+        setBalance(this.balance.subtract(funds));
+    }
 }
