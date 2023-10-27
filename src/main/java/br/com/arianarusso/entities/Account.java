9145d0091347cd4e0fc382d853fe1cfb60ab8fae
ArianaRusso <1214Unicamp!>
2023-10-27 09:47:46 -0300

    retirando construtores, gets e sets das entidades

--- src/main/java/br/com/arianarusso/entities/Account.java
+++ src/main/java/br/com/arianarusso/entities/Account.java
@@ -12,40 +12,5 @@ public class Account {
     private UUID idCustomer;
     private BigDecimal balance;
 
-    public Account(UUID id, UUID idCustomer) {
-        this.id = id;
-        this.number = generateRandomAccountNumber();
-        this.idCustomer = idCustomer;
-        this.balance = new BigDecimal(BigInteger.ZERO);
-    }
 
-    private String generateRandomAccountNumber() {
-        Random random = new Random();
-        int randomNumber = 100_000 + random.nextInt(900_000);
-        return String.valueOf(randomNumber);
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public UUID getIdCustomer() {
-        return idCustomer;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    private void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public void addFunds(BigDecimal funds){
-        setBalance(this.balance.add(funds));
-    }
-
-    public void subtractFunds(BigDecimal funds){
-        setBalance(this.balance.subtract(funds));
-    }
 }
