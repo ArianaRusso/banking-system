048c7d3c6c22fefdffe4a220834fba4e0fded1a5
ArianaRusso <1214Unicamp!>
2023-10-31 14:36:39 -0300

    alterando codigo para usar postegresql sequence

--- src/main/java/br/com/arianarusso/infra/mappers/AddressRowMapper.java
+++ src/main/java/br/com/arianarusso/infra/mappers/AddressRowMapper.java
@@ -9,7 +9,6 @@ import java.util.*;
 
 public class AddressRowMapper implements EntityMapper<Address> {
 
-    public static final String ID = "id";
     public static final String POSTAL_CODE = "postal_code";
     public static final String STREET = "street";
     public static final String NUMBER = "number";
@@ -21,7 +20,7 @@ public class AddressRowMapper implements EntityMapper<Address> {
     @Override
     public List<String> getColumnNames() {
         return Arrays.asList
-                (ID, POSTAL_CODE, STREET, NUMBER, COMPLEMENT, CITY, STATE, COUNTRY);
+                (POSTAL_CODE, STREET, NUMBER, COMPLEMENT, CITY, STATE, COUNTRY);
     }
 
     @Override
@@ -38,7 +37,6 @@ public class AddressRowMapper implements EntityMapper<Address> {
     @Override
     public Address resultSetToEntity(ResultSet resultSet) throws SQLException {
             Address address = new Address(
-                    UUID.fromString(resultSet.getString(ID)),
                     resultSet.getString(POSTAL_CODE),
                     resultSet.getString(STREET),
                     resultSet.getInt(NUMBER),
@@ -52,15 +50,13 @@ public class AddressRowMapper implements EntityMapper<Address> {
 
     @Override
     public void entityToPreparedStatement(PreparedStatement statement, Address entity) throws SQLException {
-
-        statement.setObject(1, entity.getId());
-        statement.setString(2, entity.getPostalCode());
-        statement.setString(3, entity.getStreet());
-        statement.setInt(4, entity.getNumber());
-        statement.setString(5, entity.getComplement());
-        statement.setString(6, entity.getCity());
-        statement.setString(7, entity.getState());
-        statement.setString(8, entity.getCountry());
+        statement.setString(1, entity.getPostalCode());
+        statement.setString(2, entity.getStreet());
+        statement.setInt(3, entity.getNumber());
+        statement.setString(4, entity.getComplement());
+        statement.setString(5, entity.getCity());
+        statement.setString(6, entity.getState());
+        statement.setString(7, entity.getCountry());
     }
 }
 
